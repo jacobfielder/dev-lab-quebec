248c7a207a9d254fa0a457dbfb477c97dc82e258
Fielder <jfielder@una.edu>
2025-08-26 13:59:44 -0500

    add changes

--- app.mjs
+++ app.mjs
@@ -1,9 +1,29 @@
 import express from 'express'
+import path from 'path';
 
 const app = express()
 
+const port = process.env.PORT || 3000;
+const path = require('path')
+
+app.use(express.static(__dirname + 'public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.send('Hello Express. <a href="jacob"</a>')
 })
 
-app.listen(3000)
+app.get('/jacob', (req, res) => {
+  res.send('jacob')
+})
+// endpoints... middlewares.... apis?
+// send an html file
+
+res.sendFile('jacob.html')
+
+//app.listen(3000)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+//TODO: refactor to use env port

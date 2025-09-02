30b3b966d1ba54b84148ddb40909da475a3799fa
Fielder <jfielder@una.edu>
2025-09-02 13:33:42 -0500

    new info

--- app.mjs
+++ app.mjs
@@ -7,38 +7,37 @@ import { dirname, join } from 'path';
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 //const path = require('path')
 
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-res.sendFile(join(__dirname, 'public', 'jacob.html'))
-
+app.use(express.static(join(__dirname, 'public')))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Express. <a href="jacob"</a>')
+  res.send('Hello Jacoby." <a href="jacob">jacob</a>')
 })
 
 app.get('/jacob', (req, res) => {
-  res.send('jacob')
+ 
+  res.sendFile(join(__dirname, 'public', 'jacob.html')) 
+
 })
+
+//res.sendFile(join(__dirname, 'public', 'jacob.html'))
 // endpoints... middlewares.... apis?
 // send an html file
 
-app.get('/api/barry', (req, res) => {
+app.get('/api/jacob', (req, res) => {
   // res.send('barry. <a href="/">home</a>')
   const myVar = 'Hello from server!';
   res.json({ myVar });
 })
 
-//app.listen(3000)
+app.listen(3000)
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+//app.listen(PORT, () => {
+ // console.log(`Example app listening on port ${PORT}`)
+//})
 
 //TODO: refactor to use env port

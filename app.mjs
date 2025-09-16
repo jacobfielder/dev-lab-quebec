fe95d4b7d2eca4f66509ffd1f3186fe447cc6a19
Jacob <114694870+jacobfielder@users.noreply.github.com>
2025-09-16 13:56:43 -0500

    Code for Mongo Connection

--- app.mjs
+++ app.mjs
@@ -1,14 +1,44 @@
+import 'dotenv/config'
 import express from 'express'
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 //import { dirname } from 'node:path';
 //import { fileURLToPath } from 'node:url';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 3000;
 //const path = require('path')
+
+
+
+const uri = process.env.MONGO_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.use(express.static(join(__dirname, 'public')))
